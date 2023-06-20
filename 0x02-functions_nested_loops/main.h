2753ac442a1b5920910293f3f22f390671a8a9d3
DouibaOmar <douibaomar40@gmail.com>
2023-06-20 03:02:15 -0700

    Done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,8 +4,8 @@
 /*
  * File: main.h
  * Auth: DOUIBA Omar
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops.
+ * Desc: Header file containing declarations for putchar function
+ *       used in the the first task.
  */
 
 

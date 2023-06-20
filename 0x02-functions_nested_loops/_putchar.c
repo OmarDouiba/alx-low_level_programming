5fda0e10d3702ce55aca0e5e965d8a9df63c6e95
DouibaOmar <douibaomar40@gmail.com>
2023-06-20 02:31:04 -0700

    putchar function

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+* main - check the code
+*
+* Return: On success 0.
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}

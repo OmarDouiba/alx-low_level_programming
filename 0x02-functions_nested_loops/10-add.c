2053ab5f65c5791e71729bf37388aeed7186a5cb
DouibaOmar <douibaomar40@gmail.com>
2023-06-20 08:30:18 -0700

    Done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - Adds two integers.
+* @n1: The first integer for addition.
+* @n2: The second integer for addition.
+*
+* Auth: DOUIBA Omar.
+*
+* Return: Integer value.
+*/
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

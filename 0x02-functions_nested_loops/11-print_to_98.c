35ee0803bdae8937dd92f02a0da9ff5c42b35812
DouibaOmar <douibaomar40@gmail.com>
2023-06-20 12:55:52 -0700

    D

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - Prints all natural numbers from input to 98,
+*
+* @n: The number to begin counting at.
+*
+* Auth: DOUIBA Omar
+*
+*/
+
+void print_to_98(int n)
+{
+	if (n == 0)
+	{
+		while (n <= 98)
+		{
+			if (n < 10)
+			{
+			}
+			else
+			{
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		_putchar(
+		

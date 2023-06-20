199786bf41e3e0cd86cb0c16ec82917ed250cbfa
DouibaOmar <douibaomar40@gmail.com>
2023-06-20 03:27:21 -0700

    Done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Auth : DOUIBA Omar
+ */
 void print_alphabet(void)
 {
 	char alp;
 
-	for (alp='a'; alp <= 'z'; alp++)
+	for (alp = 'a'; alp <= 'z'; alp++)
 	{
 		_putchar(alp);
 	}

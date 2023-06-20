c0af7859cc6c70624ccba40bfaa127c81610d413
DouibaOmar <douibaomar40@gmail.com>
2023-06-20 16:06:47 -0700

    Done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - Prints the first 52 fibonacci numbers
+*
+* Auth: DOUIBA Omar
+*
+* Return: Nothing!
+*/
+
+int main(void)
+{
+	long int n0 = 0, n1 = 0, n2 = 1, n3;
+
+	while (n0 <= 50)
+	{
+		n3 = n2;
+		n2 = n2 + n1;
+		n1 = n3;
+		printf("%ld", n2);
+		if (n0 != 50)
+		{
+			printf(", ");
+		}
+		n0++;
+	}
+	printf("\n");
+	return (0);
+}

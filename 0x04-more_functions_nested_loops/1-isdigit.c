4d1f2198538c54e470c7830b482517b91002c938
DouibaOmar <douibaomar40@gmail.com>
2023-06-22 02:11:05 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _isdigit - function that checks for a digit (0 through 9).
+*
+* @c: 
+*
+* Auth: DOUIBA Omar.
+*
+* Return: 1 if c is a digit, 0 otherwise.
+*/
+
+int _isdigit(int c)
+{
+	if ((c % 2) == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

ea79ca67d7de806485b767a49557cd10b60275c1
DouibaOmar <douibaomar40@gmail.com>
2023-06-22 07:49:51 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* main - function that prints a square, followed by a new line.
+*
+* @size: the size of the square
+*
+* Auth: DOUIBA Omar.
+*/
+
+void print_square(int size)
+{
+	int i = 0, j = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

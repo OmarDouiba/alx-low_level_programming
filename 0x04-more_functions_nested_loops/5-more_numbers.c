f44908d4613fc4a6ae7a2dde520a835091f59e08
DouibaOmar <douibaomar40@gmail.com>
2023-06-22 04:17:11 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* more_numbers - function that prints 10 times the numbers, from 0 to 14,
+*→      →       followed by a new line.
+*
+* Auth: DOUIBA Omar.
+*/
+
+void more_numbers(void)
+{
+	int num1 = 0, num2;
+
+	while (num1 < 10)
+	{
+		num2 = 0;
+		while (num2 <= 14)
+		{
+			if (num2 <= 9)
+			{
+				_putchar(num2 + '0');
+			}
+			else
+			{
+				_putchar((num2 / 10) + '0');
+				_putchar((num2 % 10) + '0');
+			}
+			num2++;
+		}
+		_putchar('\n');
+		num1++;
+	}
+}

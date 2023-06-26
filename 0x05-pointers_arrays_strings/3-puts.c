00ef88568531e4953a371ef1a7eb00d89a682c3b
DouibaOmar <douibaomar40@gmail.com>
2023-06-26 04:51:51 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts -  function that prints a string, followed by a new line, to stdout.
+*
+* Auth: DOUIBA Omar.
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}

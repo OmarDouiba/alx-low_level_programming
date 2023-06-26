f141f02c359e89e7b309a3b5cee05c263612ce1b
DouibaOmar <douibaomar40@gmail.com>
2023-06-26 06:05:05 -0700

    Done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,12 @@
 
 void puts2(char *str)
 {
-	char *p_str = str;
-	while (*p_str != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		_putchar(*p_str);
-		p_str += 2;
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }

df13a9ba1560a0a8411fd72fdb41b25d14023062
DouibaOmar <douibaomar40@gmail.com>
2023-06-26 04:07:25 -0700

    Done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,8 @@
 * reset_to_98 - function that takes a pointer to an int as parameter
 *		and updates the value it points to to 98.
 *
+* @n: the number that we should update.
+*
 * Auth: DOUIBA Omar.
 */
 

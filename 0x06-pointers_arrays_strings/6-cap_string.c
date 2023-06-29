6896fd177da1a8499199145f3c97fcbb55c07ad6
DouibaOmar <douibaomar40@gmail.com>
2023-06-29 15:29:26 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* *cap_string - function that capitalizes all words of a string.
+* @str: string.
+*
+* Auth: DOUIBA Omar.
+*
+* Return: capitalizes words
+*/
+
+char *cap_string(char *str)
+{
+
+	int cap_index = 1, i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	if (str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+	str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' ||
+	str[i] == ')' || str[i] == '{' || str[i] == '}')
+	{
+		cap_index = 1;
+	}
+	else if (cap_index)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+		str[i] = str[i] - 32;
+		}
+		cap_index = 0;
+	}
+	}
+	return (str);
+}

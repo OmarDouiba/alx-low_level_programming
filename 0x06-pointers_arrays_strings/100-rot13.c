a8c771bf83f20f8a3acbb2eb885390addcfe8ba5
DouibaOmar <douibaomar40@gmail.com>
2023-06-30 08:15:49 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* *rot13 - function that encodes a string using rot13.
+* @s: string.
+*
+* Auth: DOUIBA Omar.
+*/
+
+char *rot13(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'A' && s[i] <= 'M') ||
+			(s[i] >= 'a' && s[i] <= 'm'))
+		{
+			s[i] = s[i] + 13;
+		}
+		else if ((s[i] >= 'N' && s[i] <= 'Z') ||
+				(s[i] >= 'n' && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 13;
+		}
+		i++;
+	}
+	return (s);
+}

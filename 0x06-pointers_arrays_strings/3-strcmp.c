be8844094f3cf503b568dd678d4d38b2ff8c3143
OmarDouiba <73228996+OmarDouiba@users.noreply.github.com>
2023-06-30 15:05:16 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,27 +11,21 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	while (s1[i] != '\0' && s1[i] != '\0')
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] > s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
-		i++;
-	}
-
-	if (s1[i] == '\0' && s2[i] != '\0')
-	{
-		return (-1);
-	}
-	else if (s1[i] != '\0' && s2[i] == '\n')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
+		else if (s2[i] > s1[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return 0;
+		}
 	}
 }

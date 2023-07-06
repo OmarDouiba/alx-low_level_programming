32315af4af2d6d1f524d66f63d923e3032e5a9c4
DouibaOmar <douibaomar40@gmail.com>
2023-07-06 09:05:27 -0700

    Done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+* _sqrt_ - Function that returns the natural square root of a number.
+* @i: Number.
+* @m: Number.
+* Return: -1 if i * i upper then m. Otherwise sqrt.
+*/
+int _sqrt_(int m, int i)
+{
+	if (i * i > m)
+	{
+		return (-1);
+	}
+	else if (i * i == m)
+	{
+		return (i);
+	}
+	else
+	{
+		return (_sqrt_(m, ++i));
+	}
+}
+/**
+* _sqrt_recursion - Function that returns the natural square root of a number.
+* @n: Number.
+*
+* Auth: DOUIBA Omar.
+* Retuen: -1 if n negative. Otherwise sqrt.
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_(n, 0));
+	}
+}

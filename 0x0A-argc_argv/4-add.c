f387fce870c50b84c8a0c6f9c6484c1965e110b1
DouibaOmar <douibaomar40@gmail.com>
2023-07-07 10:49:44 -0700

    Done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* main - Entry point of the program.
+* @argc: Number of arguments.
+* @argv: Array of arguments.
+*
+* Auth: DOUIBA Omar.
+*
+* Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int add = 0, i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				exit(1);
+			}
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+	}
+	return (0);
+}

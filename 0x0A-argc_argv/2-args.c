970df4a803b5180e72fd854ccf2f4bb702ccf483
DouibaOmar <douibaomar40@gmail.com>
2023-07-07 09:05:41 -0700

    Done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Entry point of the program.
+* @argc: Number of arguments.
+* @argv: Array of arguments.
+*
+* Auth: DOUIBA Omar.
+*
+* Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}

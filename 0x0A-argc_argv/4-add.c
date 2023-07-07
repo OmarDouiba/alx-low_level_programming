0d7e819e6062b0b951e8036cd9a7121ac72d2f02
DouibaOmar <douibaomar40@gmail.com>
2023-07-07 10:56:10 -0700

    Done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0)
+			if (argv[i][0] >= 'a' && argv[i][0] <= 'z')
 			{
 				printf("Error\n");
 				exit(1);

3507f96e4b5bb7bf711e6c452f41889c971a412a
DouibaOmar <douibaomar40@gmail.com>
2023-07-11 06:50:50 -0700

    Done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -33,6 +33,12 @@ int **alloc_grid(int width, int height)
 	{
 		free(grid[i]);
 	}
-
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
 	return (grid);
 }

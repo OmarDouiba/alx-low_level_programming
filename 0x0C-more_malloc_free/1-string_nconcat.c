731ea1cfe45d0f0c22aa16d7e93c9be2048277e0
DouibaOmar <douibaomar40@gmail.com>
2023-07-13 11:30:28 -0700

    Done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = len_s2;
 	}
 
-	concat = malloc((len_s1 + len_s2) + 1);
+	concat = malloc((len_s1 + n) + 1);
 	if (concat == NULL)
 	{
 		return (NULL);

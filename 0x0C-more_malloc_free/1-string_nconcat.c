a289e5bddb0819752be6f4b3878c07e2d031385c
DouibaOmar <douibaomar40@gmail.com>
2023-07-13 11:35:07 -0700

    Done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+*_strlen - calculate string lenth
+*@str: string
+*
+*Return: string lenth
+*/
+int _strlen(char *str)
+{
+	int len = 0;
+
+	for (; str[len] != '\0'; len++) ;
+	return (len);
+}
+
+/**
  * *string_nconcat - concatenates n bytes of the string 2 to another string 1.
  * @s1: string 1.
  * @s2: string 2.
@@ -13,8 +27,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	unsigned int len_s1, len_s2;
 
-	len_s1 = strlen(s1);
-	len_s2 = strlen(s2);
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
 	if (s1 == NULL)
 	{
 		s1 = "";
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int _strlen(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);

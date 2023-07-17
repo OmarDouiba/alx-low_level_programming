66ee606562f72538ccfde8fb8f2d5d0da44f1668
DouibaOmar <douibaomar40@gmail.com>
2023-07-17 10:42:28 -0700

    Done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer structer
+ *
+ * Description: function that frees dogs
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}

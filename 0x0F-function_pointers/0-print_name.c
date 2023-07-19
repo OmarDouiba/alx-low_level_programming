fb52eb5c33eb7614ff2ed52ca122d9814c8a9c6c
DouibaOmar <douibaomar40@gmail.com>
2023-07-19 07:20:10 -0700

    Done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,11 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
-* print_name - Function that print a string.
-* @name: string parameter.
-* @f: Is function pointer.
-*
-* Auth: DOUIBA Omar.
+* print_name - function that prints a name
+* @name: the name to print
+* @f: the function to callback
 */
+
 void print_name(char *name, void(*f)(char *))
 {
 	if (name == NULL || f == NULL)

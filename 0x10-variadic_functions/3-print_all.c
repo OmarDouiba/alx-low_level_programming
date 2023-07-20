ac7963020ed109f8812a6e2ffacdcb3be84a4db6
DouibaOmar <douibaomar40@gmail.com>
2023-07-20 13:40:46 -0700

    Done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,45 +6,45 @@
  *print_all - Prints anything
  *@format: The conversion specifier.
  */
-void print_all(const char *const format, ...)
+void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i, n;
-	char *str;
+	int i = 0;
+	char *str, *sep = "";
 
-	n = 0;
-	while (format[n])
-		n++;
+	va_list args;
+
 	va_start(args, format);
-	i = 0;
-	while (format[i])
+
+	if (format)
 	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
-			format[i] == 's')
+		while (format[i])
 		{
 			switch (format[i])
-			{case 'c':
-					printf("%c", va_arg(args, int));
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args, int));
 					break;
 				case 'i':
-					printf("%i", va_arg(args, int));
+					printf("%s%d", sep, va_arg(args, int));
 					break;
 				case 'f':
-					printf("%f", va_arg(args, double));
+					printf("%s%f", sep, va_arg(args, double));
 					break;
 				case 's':
 					str = va_arg(args, char *);
-					if (str == NULL)
-					{
-						printf("(nil)");
-						return;
-					}
-					printf("%s", str);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
 					break;
+				default:
+					i++;
+					continue;
 			}
-			if (i != n - 1)
-				printf(", ");	}
-		i++;	}
+			sep = ", ";
+			i++;
+		}
+	}
+
 	printf("\n");
 	va_end(args);
 }

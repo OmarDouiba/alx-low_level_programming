e196c32865652c9a534de268e7a7c3b168243196
DouibaOmar <douibaomar40@gmail.com>
2023-07-20 13:29:29 -0700

    Done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,101 +5,49 @@
 /**
  *print_all - Prints anything
  *@format: The conversion specifier.
- *
- *Return: Nothing
  */
-
 void print_all(const char *const format, ...)
 {
-	int i = 0;
-	int j;
-	char *sep = "";
-
 	va_list args;
+	unsigned int i, n;
+	char *str;
 
-	fmt_t fmt[] = {
-		{ "c", print_char
-		},
-		{ "i", print_int
-		},
-		{ "f", print_float
-		},
-		{ "s", print_string
-		},
-		{
-			NULL, NULL
-		}
-	};
-
+	n = 0;
+	while (format[n])
+		n++;
 	va_start(args, format);
-
-	while (format != NULL && format[i])
+	i = 0;
+	while (format[i])
 	{
-		j = 0;
-		while (j < 4)
+		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+			format[i] == 's')
 		{
-			if (fmt[j].id[0] == format[i])
+			switch (format[i])
 			{
-				fmt[j].f(sep, args);
-				sep = ", ";
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%i", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+					{
+						printf("(nil)");
+						return;
+					}
+					printf("%s", str);
+					break;
 			}
-
-			j++;
+			if (i != n - 1)
+				printf(", ");
 		}
-
 		i++;
 	}
-
 	printf("\n");
 	va_end(args);
-}
-
-/**
- *print_char - Print a character.
- *@sep: The separator of the character
- *@args: A list of variadic arguments
- *
- */
-void print_char(char *sep, va_list args)
-{
-	printf("%s%c", sep, (char) va_arg(args, int));
-}
-
-/**
- *print_int - Print a integer.
- *@sep: The separator of the character
- *@args: A list of variadic arguments
- *
- */
-void print_int(char *sep, va_list args)
-{
-	printf("%s%i", sep, va_arg(args, int));
-}
-
-/**
- *print_float - Print a float.
- *@sep: The separator of the character
- *@args: A list of variadic arguments
- *
- */
-void print_float(char *sep, va_list args)
-{
-	printf("%s%f", sep, va_arg(args, double));
-}
-
-/**
- *print_string - Print a string.
- *@sep: The separator of the character
- *@args: A list of variadic arguments
- *
- */
-void print_string(char *sep, va_list args)
-{
-	if (va_arg(args, char *) == NULL)
-	{
-		printf("%s%s", sep, "(nil)");
-		return;
-	}
-
-	printf("%s%s", sep, va_arg(args, char *));
 }

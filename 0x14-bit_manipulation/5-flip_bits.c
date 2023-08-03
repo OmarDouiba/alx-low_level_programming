1e7d32eed970640a92935693931a806c790d68d8
DouibaOmar <douibaomar40@gmail.com>
2023-08-03 11:03:03 -0700

    Done

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - gets the number of bits flipped form n to m
+ * @n: initial lu int
+ * @m: secondary lu int
+ *
+ * Return: number of bits need to flip to get from `n to `m`
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0;
+	int mask = 1;
+	int res = n ^ m;
+
+	while (res)
+	{
+		count += res & mask;
+		res >>= 1;
+	}
+
+	return (count);
+}

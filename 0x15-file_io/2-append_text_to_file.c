65d216eb77bbfc9654df4e539b48d3eaacfb5a0a
DouibaOmar <douibaomar40@gmail.com>
2023-08-07 07:41:13 -0700

    Done

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int text_len, open_f, close_f;
+	ssize_t write_f;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+
+	open_f = open(filename, O_APPEND | O_WRONLY);
+	if (open_f == -1)
+		return (-1);
+
+	text_len = _strlen(text_content);
+	write_f = write(open_f, text_content, text_len);
+	if (write_f == -1)
+		return (-1);
+
+	close_f = close(open_f);
+	if (close_f)
+		return (-1);
+	return (1);
+}
+
+/**
+* _strlen - function that returns the length of a string.
+* @s: string.
+*
+* Auth: DOUIBA Omar.
+*
+* Return: the length of string.
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}

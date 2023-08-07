8a75dd99bb2c7bd7523f069dc9699dd20373b16c
DouibaOmar <douibaomar40@gmail.com>
2023-08-07 06:56:39 -0700

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,8 @@ int create_file(const char *filename, char *text_content)
 	int create_f, text_len, close_f;
 	ssize_t write_f;
 
+	if (text_content == NULL)
+		text_content = "";
 	if (filename == NULL)
 		return (-1);
 

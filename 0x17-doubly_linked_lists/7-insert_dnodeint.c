3eaf9fc6622ce78b3fa98d9377ffa7c217ccc431
DouibaOmar <douibaomar40@gmail.com>
2023-09-15 08:55:00 -0700

    Done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,44 +1,51 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * insert_dnodeint_at_index - function that inserts a new node at a given index
+ * of a linked dlistint_t list
+ * @h: double pointer, pointer to pointer to head node defined in main
+ * @idx: index of the node where the insertion is made
+ * @n: value to be staged at new node
+ * Return: pointer to the new node inserted at index
+ */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *tmp, *tmp1;
+	unsigned int count = 0;
+	dlistint_t *temp = NULL;
+	dlistint_t *new = NULL;
 
-	if (!h || !(*h))
-		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
+	temp = *h;
+	while (temp != NULL)
+	{
+		if (count != idx)
+			temp = temp->next, count++;
+		else
+		{
+			if (count == 0)
+				new = add_dnodeint(h, n);
+			else
+			{
+				new = malloc(sizeof(dlistint_t));
+				if (new == NULL)
+					return (NULL);
+				new->n = n;
 
-	if (idx == 0)
-	{
-		new = malloc(sizeof(dlistint_t));
-		new->next = (*h);
-		(*h)->prev = new;
-		(*h) = new;
-		return (*h);
+				new->next = temp;
+				new->prev = temp->prev;
+				temp->prev = new;
+				temp = new->prev;
+				temp->next = new;
+			}
+			return (new);
+		}
 	}
-	
-	tmp = (*h);
-	while (tmp && idx > 1)
-	{
-		tmp = tmp->next;
-		idx--;
-	}
-	if (tmp && idx == 0)
-	{
-		new = malloc(sizeof(dlistint_t));
-		tmp1 = tmp->next;
-		tmp->next = new;
-		new->prev = tmp;
-		new->next = tmp1;
-		tmp1->prev = new;
-		return (new);
-	}
-	else if (tmp->next == NULL && idx == 1)
+	if (idx == count)
 	{
 		new = add_dnodeint_end(h, n);
-		tmp->next = new;
-		new->prev = tmp;
 		return (new);
 	}
 	return (NULL);

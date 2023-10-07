aa03000f7f88c6d616acc954f51c40ead80013b8
DouibaOmar <douibaomar40@gmail.com>
2023-10-07 05:41:10 -0700

    Done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,23 +7,24 @@
  * Return: The value of the key if it found, otherwise NULL.
  */
 
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *tmp;
 	unsigned long int index = 0;
 
-	if (ht || key)
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size);
+
+	tmp = ht->array[index];
+	while (tmp)
 	{
-		index = key_index((const unsigned char *) key, ht->size);
-
-		tmp = ht->array[index];
-		while (tmp)
-		{
-			if (strcmp(tmp->key, key) == 0)
-				return (tmp->value);
-
-			tmp = tmp->next;
-		}
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
 	}
+
 	return (NULL);
 }

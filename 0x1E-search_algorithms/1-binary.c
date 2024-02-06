7fcfe06ba041861972add4ee57239407e1245b72
DouibaOmar <douibaomar40@gmail.com>
2024-02-06 13:09:00 +0100

    Done

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,40 +10,4 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t l = 0, r = size, i;
-    int mid;
-
-    if (array == NULL)
-        return -1;
-
-    while (l <= r)
-    {
-        mid = l + (r - l) / 2;
-
-        printf("Searching in array: ");
-        for (i = l; i < r; i++)
-        {
-            printf("%ld", i);
-            if (i != r - 1)
-                printf(", ");
-            else
-            {
-                printf("\n");
-            }
-        }
-
-        if (array[mid] == value)
-        {
-            return (mid);
-        }
-        if (value < array[mid])
-        {
-            r = mid - 1;
-        }
-        else
-        {
-            l = mid + 1;
-        }
-    }
-    return (-1);
 }
